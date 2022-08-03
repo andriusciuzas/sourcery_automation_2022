19eb80a04401f632ef7f82bb8dd23ce33170626e
ASUS\User <margarita.cernych@gmail.com>
2022-08-03 10:56:40 +0300

    added const and added basicFunctionTest function

--- homework/tests/homework.spec.js
+++ homework/tests/homework.spec.js
@@ -3,61 +3,73 @@ const { test, expect } = require("@playwright/test");
 
 const data = ["Prototype", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+const
+positiveInteger = '8',
+positiveFloat = '4.6',
+zero = '0',
+negativeInteger = '-4',
+negativeFloat = '-7.3',
+text = 'gh'
+
 test.beforeEach(async ({ page }) => {
   await page.goto("https://testsheepnz.github.io/BasicCalculator");
 });
 
 data.forEach((build) => {
   test.describe(build + ": " + "Action: Concatenate", () => {
-    basicFunctions(build, "Concatenate", "2", "3", false, "23");
-    basicFunctions(build, "Concatenate", "6", "-7", false, "6-7");
+    chooseBuild(build,);
+    enterFirstAndSecondNumbers(firstNumbet, secondNumber);
+
   });
 
-  test.describe(build + ": " + "Action: Add", () => {
-    basicFunctions(build, "Add", "2", "3.4", true, "5");
-    basicFunctions(build, "Add", "-1", "0", false, "-1");
-  });
-
-  test.describe(build + ": " + "Action: Subtract", () => {
-    basicFunctions(build, "Subtract", "4.2", "8.6", false, "-4.4");
-    basicFunctions(build, "Subtract", "8.6", "4.2", true, "4");
-  });
-
-  test.describe(build + ": " + "Action: Multiply", () => {
-    basicFunctions(build, "Multiply", "8.68", "10", true, "86");
-    basicFunctions(build, "Multiply", "8.68", "10", false, "86.8");
-  });
-
-  test.describe(build + ": " + "Action: Divide", () => {
-    basicFunctions(build, "Divide", "9", "4", false, "2.25");
-    basicFunctions(build, "Divide", "9", "4", true, "2");
-    basicFunctions(build, "Divide", "50", "-2", false, "-25");
-  });
-
-  test.describe(
-    build + ": Double Calculate button click wokrs properly",
-    () => {
-      doubleCalculateButtonClick(build, "Add", "5", "7", "12");
-    }
-  );
-
-  test.describe(build + ": Check Clear button", () => {
-    clearButtonWorks(build, "Add", "2", "3", "");
-  });
-
-  test.describe(build + ": Error messages test", () => {
-    correctErrorMessage(build, "Add", "gh", "3", "Number 1 is not a number");
-    correctErrorMessage(
-      build,
-      "Multiply",
-      "5",
-      "df",
-      "Number 2 is not a number"
+  /**
+   * @param {string} build
+   * @param {string} action
+   * @param {string} firstNumbet
+   * @param {string} secondNumber
+   * @param {boolean} onlyIntegers
+   * @param {string} result
+   */
+  function basicFunctions(build, action, firstNumbet, secondNumber, onlyIntegers, result) {
+    test(
+      build +
+        " Action: " +
+        action +
+        " " +
+        firstNumbet +
+        " and " +
+        secondNumber +
+        ". Result: " +
+        result +
+        ". Only integers: " +
+        onlyIntegers,
+      async ({ page }) => {
+        /**
+         * @param {any} build
+         */
+        function chooseBuild(build);
+        /**
+         * @param {any} firstNumber
+         * @param {any} secondNumber
+         */
+        function enterFirstAndSecondNumbers(firstNumber, secondNumber);
+        /**
+         * @param {any} action
+         */
+        function selectOperation(action);
+        /**
+         * @param {any} onlyIntegers
+         */
+        function integersOnly(onlyIntegers);
+        function clickCalculateButton();
+        /**
+         * @param {any} result
+         */
+        function getResult(result);
+      }
     );
-    correctErrorMessage(build, "Divide", "78", "0", "Divide by zero error!");
-    correctErrorMessage(build, "Divide", "78", "", "Divide by zero error!");
-  });
-});
+  }
+
 
 /**
  * @param {string} build
@@ -110,20 +122,11 @@ function getResult(result) {
   });
 }
 
-/**
- * @param {string} build
- * @param {string} action
- * @param {string} number1
- * @param {string} number2
- * @param {string} result
- */
-function doubleCalculateButtonClick(build, action, number1, number2, result) {
-    async ({ page }) => {
-      await page.locator("#calculateButton").dblclick();
-    }
-  ;
+function doubleCalculateButtonClick() {
+  async ({ page }) => {
+    await page.locator("#calculateButton").dblclick();
+  };
 }
-
 
 function clickClearButton() {
   test("Clear button works.", async ({ page }) => {
